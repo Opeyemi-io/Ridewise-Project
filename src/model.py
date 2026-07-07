057ec5f0eea1dccb3ebb508721821d659ae68ad3
Opeyemi-io <opeyemiadegoke3@gmail.com>
2026-07-07 11:45:35 -0400

    model

--- src/model.py
+++ src/model.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.metrics import roc_auc_score, classification_report, confusion_matrix
 import mlflow
 import mlflow.sklearn
@@ -26,6 +28,35 @@ with mlflow.start_run(run_name="Logistic Regression") as run:
     auc_score = roc_auc_score(y_test, y_prob)
     
     mlflow.log_param("model_type", "LogisticRegression")
+    mlflow.log_param("features", x.columns.tolist())
+    mlflow.log_param("mi_threshold", 0.000001)
+    mlflow.log_metric("roc_auc", auc_score)
+    mlflow.sklearn.log_model(model, "model")
+
+    print(f"Run ID: {run.info.run_id}")
+    print(f"ROC AUC Score: {auc_score}")
+
+with mlflow.start_run(run_name="Random Forest") as run:
+param_dist = {
+    "n_estimators":     [100, 200, 300, 500],
+    "max_depth":        [None, 5, 10, 20, 30],
+    "min_samples_split":[2, 5, 10],
+    "min_samples_leaf": [1, 2, 3]
+}
+rf_search = RandomizedSearchCV(RandomForestClassifier(random_state=42, class_weight='balanced'),
+        param_distributions=param_dist,
+        scoring="roc_auc",
+        cv=5,
+        random_state=42)
+
+    model = rf_search.best_estimator_
+    rf_search.fit(x_train, y_train)
+
+    y_pred = model.predict(x_test)
+    y_prob = model.predict_proba(x_test)[:, 1] 
+    auc_score = roc_auc_score(y_test, y_prob)
+    
+    mlflow.log_param("model_type", "RandomForestClassifier")
     mlflow.log_param("features", x.columns.tolist())
     mlflow.log_param("mi_threshold", 0.000001)
     mlflow.log_metric("roc_auc", auc_score)
